c4b81b869fcd525c50241688fb7f85c0a52678fe
PerpetuumG <perpetuumg@mail.ru>
2020-06-15 10:33:51 +0300

    Создание верхнего меню footer

--- gulpfile.js
+++ gulpfile.js
@@ -14,84 +14,85 @@ const svgmin = require('gulp-svgmin');
 const cheerio = require('gulp-cheerio');
 const replace = require('gulp-replace');
 
+
 gulp.task('sass', function () {
   return gulp.src('scss/style.scss')
-      .pipe(plumber())
-      .pipe(sourceMaps.init())
-      .pipe(sass())
-      .pipe(autoprefixer({
-        browsers: ['last 2 version']
-      }))
-      .pipe(sourceMaps.write())
-      .pipe(gulp.dest('build/css'))
-      .pipe(browserSync.reload({stream: true}));
+    .pipe(plumber())
+    .pipe(sourceMaps.init())
+    .pipe(sass())
+    .pipe(autoprefixer({
+      browsers: ['last 2 version']
+    }))
+    .pipe(sourceMaps.write())
+    .pipe(gulp.dest('build/css'))
+    .pipe(browserSync.reload({ stream: true }));
 });
 
 gulp.task('html', function () {
   return gulp.src('*.html')
-      .pipe(gulp.dest('build'))
-      .pipe(browserSync.reload({stream: true}));
+    .pipe(gulp.dest('build'))
+    .pipe(browserSync.reload({ stream: true }));
 });
 
 gulp.task('js', function () {
   return gulp.src('js/**/*.js')
-      .pipe(gulp.dest('build/js'))
-      .pipe(browserSync.reload({stream: true}));
+    .pipe(gulp.dest('build/js'))
+    .pipe(browserSync.reload({ stream: true }));
 });
 
 gulp.task('css', function () {
   return gulp.src('css/**/*.css')
-      .pipe(gulp.dest('build/css'))
-      .pipe(browserSync.reload({stream: true}));
+    .pipe(gulp.dest('build/css'))
+    .pipe(browserSync.reload({ stream: true }));
 });
 
 gulp.task('allimg', function () {
   return gulp.src('img/**/*.{png,jpg}')
-      .pipe(gulp.dest('build/img'))
-      .pipe(browserSync.reload({stream: true}));
+    .pipe(gulp.dest('build/img'))
+    .pipe(browserSync.reload({ stream: true }));
 });
 
 gulp.task('images', function () {
   return gulp.src('build/img/**/*.{png,jpg}')
-      .pipe(imagemin([
-          imagemin.jpegtran({progressive: true}),
-          imageminJpegRecompress({
-            loops: 5,
-            min: 65,
-            max: 70,
-            quality: 'medium'
-          }),
-          imagemin.optipng({optimizationLevel: 3}),
-          pngquant({quality: '65-70', speed: 5})
-      ]))
-      .pipe(gulp.dest('build/img'));
+    .pipe(imagemin([
+      imagemin.jpegtran({ progressive: true }),
+      imageminJpegRecompress({
+        loops: 5,
+        min: 65,
+        max: 70,
+        quality: 'medium'
+      }),
+      imagemin.optipng({ optimizationLevel: 3 }),
+      pngquant({ quality: '65-70', speed: 5 })
+    ]))
+    .pipe(gulp.dest('build/img'));
 });
 
 gulp.task('svg', function () {
   return gulp.src('img/**/*.svg')
-      .pipe(svgmin({
-        js2svg: {
-          pretty: true
+    .pipe(svgmin({
+      js2svg: {
+        pretty: true
+      }
+    }))
+    .pipe(cheerio({
+      run: function ($) {
+        $('[fill]').removeAttr('fill');
+        $('[stroke]').removeAttr('stroke');
+        $('[style]').removeAttr('style');
+      },
+      parserOptions: { xmlMode: true }
+    }))
+    .pipe(replace('&gt;', '>'))
+    // build svg sprite
+    .pipe(svgSprite({
+      mode: {
+        symbol: {
+          sprite: "sprite.svg"
         }
-      }))
-      .pipe(cheerio({
-        run: function ($) {
-          $('[fill]').removeAttr('fill');
-          $('[stroke]').removeAttr('stroke');
-          $('[style]').removeAttr('style');
-        },
-        parserOptions: {xmlMode: true}
-      }))
-      .pipe(replace('&gt;', '>'))
-      // build svg sprite
-      .pipe(svgSprite({
-        mode: {
-          symbol: {
-            sprite: "sprite.svg"
-          }
-        }
-      }))
-      .pipe(gulp.dest('build/img'));
+      }
+    }))
+    .pipe(gulp.dest('build/img'));
 });
 
 gulp.task('serve', function () {
@@ -109,14 +110,14 @@ gulp.task('serve', function () {
 
 gulp.task('copy', function () {
   return gulp.src([
-      'img/**',
-      'js/**',
-      'css/**',
-      '*.html'
+    'img/**',
+    'js/**',
+    'css/**',
+    '*.html'
   ], {
     base: '.'
   })
-      .pipe(gulp.dest('build'));
+    .pipe(gulp.dest('build'));
 
 });
 
@@ -134,14 +135,3 @@ gulp.task('build', function (fn) {
     fn
   );
 });
-
-
-
-
-
-
-
-
-
-
-
